7619a2539b2c14234cf65032d6d4a69a34d72933
John Patrick Monroyo <monroyojohnpatrick@gmail.com>
2025-11-24 15:29:46 +0800

    Delete database directory

--- database/add_tricycle_number.sql
+++
@@ -1,6 +0,0 @@
--- Add tricycle_number column to tricycle_drivers table
-ALTER TABLE tricycle_drivers 
-ADD COLUMN tricycle_number VARCHAR(20) UNIQUE AFTER plate_number;
-
--- Create index for tricycle_number
-CREATE INDEX idx_tricycle_number ON tricycle_drivers(tricycle_number);
